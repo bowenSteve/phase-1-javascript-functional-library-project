5f286816d3317857b257379685a456d4aa83f328
Stephen Bowen <bowensteven17@gmail.com>
2024-04-16 10:07:10 +0300

    Javascript functional library project

---
+++ index.js
@@ -0,0 +1,110 @@
+let convertToArray = (data) => {
+    if (typeof (data) === 'object') {
+        return [...Object.values(data)];
+    } else {
+        return [data];
+    }
+}
+function myEach(collection, callback){
+    let col = convertToArray(collection);
+ for (let i = 0; i < col.length; i++) {
+        callback(col[i]);
+    }
+    return collection;
+}
+
+function myMap(collection, callback){
+    let col = convertToArray(collection);
+    let arr = [];
+
+    for (let i = 0; i < col.length; i++) {
+        arr.push(callback(col[i]));
+    }
+    return arr;
+}
+
+function myReduce(collection, callback, acc) {
+    let col = convertToArray(collection);
+
+    if (arguments.length === 2) {
+        acc = col[0];
+        col.shift();
+    }
+
+    for (let i = 0; i < col.length; i++) {
+        acc = callback(acc, col[i]);
+    }
+    return acc;
+}
+
+function myFind(collection, callback) {
+    let col = convertToArray(collection);
+    for (let i = 0; i < col.length; i++) {
+        if (callback(col[i])) {
+            return col[i];
+        }
+    }
+}
+
+function myFilter(collection, callback) {
+    let col = convertToArray(collection);
+    let passedValues = [];
+
+    for (let i = 0; i < col.length; i++) {
+        if (callback(col[i])) {
+            passedValues.push(col[i]);
+        }
+    }
+    return passedValues;
+}
+
+function mySize(collection) {
+    let size = 0;
+    if (typeof(collection) === 'object') {
+        size = Object.keys(collection).length;
+    } else {
+        size = collection.length;
+    }
+    return size;
+}
+
+function myFirst(arr, n) {
+    if (arguments.length === 2) {
+        return arr.slice(0, n);
+    } else {
+        return arr[0];
+    }
+}
+
+function myLast(arr, n) {
+    if (arguments.length === 2) {
+        return arr.slice(-n);
+    } else {
+        return arr[arr.length - 1];
+    }
+}
+
+function mySortBy(data, cb) {
+    let arr = [];
+    for (let i = 0; i < data.length; i++) {
+        arr.push(cb(data[i]));
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+function myKeys(obj) {
+    return Object.keys(obj);
+}
+
+function myValues(obj) {
+    return Object.values(obj);
+}
